74a99fb5fe21b5cbb0aa0c365b200a1f582507dd
NvDevelops <167025563+NvDevelops@users.noreply.github.com>
2025-05-15 09:58:17 -0700

    e

--- First Game/scripts/game_manager.gd
+++ First Game/scripts/game_manager.gd
@@ -1,9 +1,23 @@
 extends Node
 
 var score = 0
+var time_elapsed = 0.0
+var timer_running = true
 
 @onready var score_label = $ScoreLabel
+@onready var timer_label = $TimerLabel
+
+func _process(delta):
+	if timer_running:
+		time_elapsed += delta
+		timer_label.text = "Time: " + str("%.2f" % time_elapsed) + "s"
 
 func add_point():
 	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
+	score_label.text = "You collected " + str(score) + " coins out of 18."
+
+# Called when the player enters the finish area
+func _on_finish_area_body_entered(body):
+	if body.name == "Player":  # Adjust 'Player' if your node is named differently
+		timer_running = false
+		timer_label.text += "  (Finished!)"
--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -350.0
 const DASH_SPEED = 400.0
 const DASH_DURATION = 0.15
 const DASH_COOLDOWN = 0.5
-const GRAVITY = 980.0  # Fixed gravity value
+const GRAVITY = 980.0
 const ACCELERATION = 12.0
 const DECELERATION = 8.0
 const MAX_SPEED = 150.0
@@ -15,16 +15,14 @@ const MAX_SPEED = 150.0
 var dash_time = 0.0
 var dash_cooldown = 0.0
 var can_dash = true
-var is_wall_sliding = false
 
-# Nodes
+# Mobile Input Tracking
+var left_pressed = false
+var right_pressed = false
+var jump_pressed = false
+var dash_pressed = false
+
 @onready var animated_sprite = $AnimatedSprite2D
-# Removed wall checks by default, add these back only if you set up the nodes
-#@onready var wall_check_left = $WallCheckLeft
-#@onready var wall_check_right = $WallCheckRight
-
-func _ready():
-	set_physics_process(true)
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -32,13 +30,17 @@ func _physics_process(delta):
 
 	handle_movement(delta)
 	handle_dash(delta)
-	# handle_wall_slide(delta)  # Disabled by default
-
 	move_and_slide()
 	handle_animations()
 
 func handle_movement(delta):
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction = 0
+	if left_pressed and not right_pressed:
+		direction = -1
+	elif right_pressed and not left_pressed:
+		direction = 1
+	else:
+		direction = Input.get_axis("move_left", "move_right")
 
 	if direction != 0:
 		velocity.x = lerp(velocity.x, float(direction) * MAX_SPEED, ACCELERATION * delta)
@@ -46,9 +48,10 @@ func handle_movement(delta):
 	else:
 		velocity.x = lerp(velocity.x, 0.0, DECELERATION * delta)
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	var jump_now = Input.is_action_just_pressed("jump") or jump_pressed
+	if jump_now and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	# Wall jumping removed for now (needs wall slide)
+		jump_pressed = false # For mobile: only jumps once per tap
 
 func handle_dash(delta):
 	if dash_cooldown > 0:
@@ -56,27 +59,20 @@ func handle_dash(delta):
 		if dash_cooldown <= 0:
 			can_dash = true
 
-	if can_dash and Input.is_action_just_pressed("dash"):
+	var dash_now = Input.is_action_just_pressed("dash") or dash_pressed
+	if can_dash and dash_now:
 		var dash_direction = 1 if not animated_sprite.flip_h else -1
 		velocity.x = dash_direction * DASH_SPEED
 		dash_time = DASH_DURATION
 		dash_cooldown = DASH_COOLDOWN
 		can_dash = false
 		animated_sprite.play("dash")
+		dash_pressed = false
 
 	if dash_time > 0:
 		dash_time -= delta
 		if dash_time <= 0:
 			velocity.x = 0
-
-# Wall slide removed for now
-#func handle_wall_slide(delta):
-#    var touching_wall = (wall_check_left.is_colliding() and velocity.x < 0) or (wall_check_right.is_colliding() and velocity.x > 0)
-#    if touching_wall and not is_on_floor():
-#        is_wall_sliding = true
-#        velocity.y = min(velocity.y, GRAVITY * 0.5)
-#    else:
-#        is_wall_sliding = false
 
 func handle_animations():
 	if is_on_floor():
@@ -88,3 +84,24 @@ func handle_animations():
 		animated_sprite.play("dash")
 	else:
 		animated_sprite.play("jump")
+
+# BUTTON SIGNALS (connect these in the editor for each button)
+func _on_left_button_button_down():
+	left_pressed = true
+func _on_left_button_button_up():
+	left_pressed = false
+
+func _on_right_button_button_down():
+	right_pressed = true
+func _on_right_button_button_up():
+	right_pressed = false
+
+func _on_jump_button_button_down():
+	jump_pressed = true
+func _on_jump_button_button_up():
+	jump_pressed = false
+
+func _on_dash_button_button_down():
+	dash_pressed = true
+func _on_dash_button_button_up():
+	dash_pressed = false
